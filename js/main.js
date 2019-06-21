c9949ecc8f9a2a427e7f90ac68a4cc762f8eb530
miaranarakoto <miaranarakoto@users.noreply.github.com>
2019-06-21 17:44:48 +0200

    m

---
+++ js/main.js
@@ -0,0 +1,10 @@
+for (var i=1; i<50; i++)
+{
+  document.write("<h1> testando parallax +i+ </h1>");
+}
+
+//  const parallax = document.getElementById ("parallax");
+//  window.addEventListener ("scroll", function () {
+//    let offset = window.pageYOffset;
+//    parallax.style.backgroundPositionY = offset*0.7 + "px";
+//  })
